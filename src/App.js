b014ed146b4ea4437354e0bf1d451ac82a690a62
Godfrey Bongomin <bongomin@uw.edu>
2022-08-22 11:24:51 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">this is it</header>
+    </div>
+  );
+}
+
+export default App;
